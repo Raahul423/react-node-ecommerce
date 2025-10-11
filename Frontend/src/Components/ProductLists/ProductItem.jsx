175f6fc7fe5b8866ec85dd0d0c43ecd1b4b4cb67
Raahul423 <rpal24480@gmail.com>
2025-10-12 00:47:02 +0530

    working in products routes

--- Frontend/src/Components/ProductLists/ProductItem.jsx
+++ Frontend/src/Components/ProductLists/ProductItem.jsx
@@ -1,29 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SelectedItems from './SubProductSlider/SelectedItems'
 import { HiOutlineBars3 } from "react-icons/hi2";
-import { PiDropboxLogo } from "react-icons/pi";
+import { IoGrid } from "react-icons/io5";
+import { Button } from '@mui/material';
 
 
 const ProductItem = () => {
 
+  const [itemveiw, setItemveiw] = useState('grid');
+
+  console.log(itemveiw);
+
+
   return (
     <section className='col1 w-[80%]'>
-      <div  className='1/2 bg-gray-800/20 p-4 flex justify-between rounded-md sticky z-100 top-0'>
-        <div className='flex items-center gap-4'>
-          <HiOutlineBars3 />
-          <PiDropboxLogo />
-          <p>Their are 8 products</p>
+      <div className='1/2 bg-[#f2f2f2] p-3 flex justify-between rounded-md sticky z-100 top-0'>
+        <div className='flex items-center gap-2 itemveiw'>
+          <Button onClick={() => setItemveiw('list')} className={`!rounded-full !min-w-[35px] h-[35px] ${itemveiw === 'list' && 'active'}`}>
+            <HiOutlineBars3  className='text-xl text-gray-900/80 active'/>
+          </Button >
 
+          <Button onClick={() => setItemveiw('grid')} className={`!rounded-full !min-w-[35px] h-[35px] ${itemveiw === 'grid' && 'active'}`}>
+            <IoGrid  className='text-xl text-gray-900/80' />
+          </Button>
+
+          <p className='text-gray-900/80'>Their are 8 products</p>
 
         </div>
-        hi
-        <div>
 
+        <div>
+          hii
         </div>
       </div>
 
       <div className='2/2 w-full'>
-        <SelectedItems />
+        {itemveiw === 'grid' ? <SelectedItems />:''}
       </div>
     </section>
   )
