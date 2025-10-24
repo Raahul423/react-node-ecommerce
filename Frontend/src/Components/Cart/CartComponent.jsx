90369e69e33dc83841e5faf49b73497197695623
Raahul423 <rpal24480@gmail.com>
2025-10-24 23:20:22 +0530

    fix some issue in Cart Section

--- Frontend/src/Components/Cart/CartComponent.jsx
+++ Frontend/src/Components/Cart/CartComponent.jsx
@@ -1,16 +1,38 @@
 import { Button, Rating } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 import { RxCross2 } from "react-icons/rx";
 import { Link } from 'react-router';
+import { IoMdArrowDropdown } from "react-icons/io";
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 const CartComponent = () => {
+
+    const [sizeanchorEl, setSizeAnchorEl] = useState(null);
+    const [size, setSize] = useState("S");
+    const open = Boolean(sizeanchorEl);
+    const handleClick = (event) => {
+        setSizeAnchorEl(event.currentTarget);
+    };
+    const handleClose = (value) => {
+        setSize(value)
+        setSizeAnchorEl(null);
+    };
+
+
+
     return (
         <section className='my-container flex px-12 py-10 gap-6'>
             <div className='col1 w-[70%] border border-gray-600/10 rounded-md shadow-sm shadow-gray-700/80 bg-white'>
+
+                {/* Heading Part of Cart section */}
                 <div className='p-4'>
                     <h1>Your Cart</h1>
                     <p>There are <span className='text-red-600'>4</span> products in your cart</p>
                 </div>
+
+
+                {/* data section of cart section using loop */}
 
                 <div className='py-1'>
                     {Array.from({ length: 4 }).map((idx) => (
@@ -20,10 +42,31 @@ const CartComponent = () => {
                                     <img className='rounded-md hover:scale-110 transition-all ease-in-out cursor-pointer' src="https://serviceapi.spicezgold.com/download/1742462909156_gdgd1.jpg" alt="Error" />
                                 </div>
 
-                                <div>
+                                <div className='flex flex-col gap-1 justify-center'>
                                     <p className='!text-sm'>Campus Sutra</p>
+
                                     <h1 className='!text-xl'>Men Comfort Cuban Collar Solid Polycotton Casual Shirt...</h1>
+
                                     <Rating name="half-rating-read" defaultValue={4} precision={1} />
+
+                                    <div className='px-2 flex items-center py-1 bg-[#f1f1f1] w-fit rounded-md'>
+                                        <span onClick={()=>handleClick(idx)} className='text-sm flex items-center gap-1 cursor-pointer'>Size: {size} <IoMdArrowDropdown /></span>
+
+                                        <Menu
+                                            id="size-menu"
+                                            anchorEl={sizeanchorEl}
+                                            open={open}
+                                            onClose={handleClose}
+
+                                        >
+                                            <MenuItem onClick={() => handleClose("S")}>S</MenuItem>
+                                            <MenuItem onClick={() => handleClose("M")}>M</MenuItem>
+                                            <MenuItem onClick={() => handleClose("L")}>L</MenuItem>
+                                            <MenuItem onClick={() => handleClose("XL")}>XL</MenuItem>
+                                            <MenuItem onClick={() => handleClose("XXL")}>XXL</MenuItem>
+                                        </Menu>
+                                    </div>
+
                                     <div className='flex gap-4'>
                                         <p>₹1350</p>
                                         <p className='line-through text-gray-600/70'>₹1450</p>
@@ -42,6 +85,9 @@ const CartComponent = () => {
                 </div>
             </div>
 
+
+
+            {/* Cart Totals Sections  */}
 
             <div className='col1 w-[30%]  px-6 border border-gray-600/10 rounded-md shadow-sm shadow-gray-700/80 bg-white h-fit sticky top-60'>
                 <div className='border-gray-700/60 border-b-1 py-3 '>
