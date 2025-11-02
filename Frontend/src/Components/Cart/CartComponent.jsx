d221969441b1a9cb05a08fd4fc933f97d9514a7e
Raahul423 <rpal24480@gmail.com>
2025-11-02 15:59:37 +0530

    cart component item fix some bugs

--- Frontend/src/Components/Cart/CartComponent.jsx
+++ Frontend/src/Components/Cart/CartComponent.jsx
@@ -1,13 +1,52 @@
 import { Button } from '@mui/material'
 import { Link } from 'react-router';
 import SizeComponent from './Size/SizeComponent';
+import { useState } from 'react';
 
 
 const CartComponent = () => {
+    const [cartItem, setCartItem] = useState([
+        {
+            id: 1,
+            name: "Campus Sutra",
+            title: "Men Comfort Cuban Collar Solid Polycotton Casual Shirt...",
+            price: 1350,
+            originalPrice: 1450,
+            size: "S",
+            img: "https://serviceapi.spicezgold.com/download/1742462909156_gdgd1.jpg",
+        },
+        {
+            id: 2,
+            name: "Levis",
+            title: "Men Regular Fit Cotton Casual Shirt...",
+            price: 1750,
+            originalPrice: 1950,
+            size: "M",
+            img: "https://serviceapi.spicezgold.com/download/1742462909156_gdgd1.jpg",
+        },
+    ])
+
+
+    const sizecomponent = (id, setsize) => {
+        setCartItem((prev) => {
+            return prev.map((item) => {
+                return item.id === id ? { ...item, size: setsize } : item
+            })
+        })
+    }
+
+
+    const removeItem = (id) => {
+        setCartItem((prev) => {
+            return prev.filter((item) => {
+                return item.id !== id
+            })
+        })
+    }
 
     return (
         <section className='my-container flex px-12 py-10 gap-6'>
-            <div className='col1 w-[70%] border border-gray-600/10 rounded-md shadow-sm shadow-gray-700/80 bg-white'>
+            <div className='col1 w-[70%] border border-gray-600/10 rounded-md shadow-sm shadow-gray-700/80 bg-white h-fit'>
 
                 {/* Heading Part of Cart section */}
                 <div className='p-4'>
@@ -19,8 +58,14 @@ const CartComponent = () => {
                 {/* data section of cart section  */}
 
                 <div>
-                    <SizeComponent productsize={"S"} />
-                    <SizeComponent productsize={"S"} />
+                    {cartItem.map((item) => (
+                        <SizeComponent
+                            key={item.id}
+                            item={item}
+                            onsizeChange={sizecomponent}
+                            onremove={removeItem}
+                        />
+                    ))}
                 </div>
 
 
--- Frontend/src/Components/Cart/Size/SizeComponent.jsx
+++ Frontend/src/Components/Cart/Size/SizeComponent.jsx
@@ -5,73 +5,69 @@ import MenuItem from '@mui/material/MenuItem';
 import { Rating } from '@mui/material';
 import { RxCross2 } from "react-icons/rx";
 
-const SizeComponent = ({ productsize }) => {
-
+const SizeComponent = ({ item, onsizeChange, onremove }) => {
     const [sizeanchorEl, setSizeAnchorEl] = useState(null);
-    const [size, setSize] = useState(productsize);
     const open = Boolean(sizeanchorEl);
 
     const handleClick = (event) => {
         setSizeAnchorEl(event.currentTarget);
-
     };
 
-    const handleClose = (item) => {
+    const handleClose = (newSize) => {
         setSizeAnchorEl(null);
-        setSize(item)
+        if(newSize) onsizeChange(item.id, newSize)
     };
-
 
 
     return (
-            <section className='py-1'>
-                <div className='flex justify-between border-t-1 border-gray-800/40 border-b-1 p-4'>
-                    <div className='flex gap-4'>
-                        <div className='w-25 rounded-md overflow-hidden '>
-                            <img className='rounded-md hover:scale-110 transition-all ease-in-out cursor-pointer' src="https://serviceapi.spicezgold.com/download/1742462909156_gdgd1.jpg" alt="Error" />
-                        </div>
-
-                        <div className='flex flex-col gap-1 justify-center'>
-                            <p className='!text-sm'>Campus Sutra</p>
-
-                            <h1 className='!text-xl'>Men Comfort Cuban Collar Solid Polycotton Casual Shirt...</h1>
-
-                            <Rating name="half-rating-read" defaultValue={4} precision={1} />
-
-                            <div className='px-2 flex items-center py-1 bg-[#f1f1f1] w-fit rounded-md'>
-                                <span onClick={handleClick} className='text-sm flex items-center gap-1 cursor-pointer'>Size: {size} <IoMdArrowDropdown /></span>
-
-                                <Menu
-                                    id="size-menu"
-                                    anchorEl={sizeanchorEl}
-                                    open={open}
-                                    onClose={() => handleClose(null)}
-
-                                >
-                                    {["S", "M", "L", "XL", "XXL"].map((item) => (
-                                        <MenuItem key={item} onClick={() => handleClose(item)}>
-                                            {item}
-                                        </MenuItem>
-                                    ))}
-                                </Menu>
-                            </div>
-
-                            <div className='flex gap-4'>
-                                <p>₹1350</p>
-                                <p className='line-through text-gray-600/70'>₹1450</p>
-                                <p className='text-red-600'>
-                                    19% OFF
-                                </p>
-                            </div>
-                        </div>
+        <section className='py-1'>
+            <div className='flex justify-between border-t-1 border-gray-700/50 p-4'>
+                <div className='flex gap-4'>
+                    <div className='w-25 rounded-md overflow-hidden '>
+                        <img className='rounded-md hover:scale-110 transition-all ease-in-out cursor-pointer' src="https://serviceapi.spicezgold.com/download/1742462909156_gdgd1.jpg" alt="Error" />
                     </div>
 
-                    <div>
-                        <RxCross2 className='text-2xl cursor-pointer' />
+                    <div className='flex flex-col gap-1 justify-center'>
+                        <p className='!text-sm'>Campus Sutra</p>
+
+                        <h1 className='!text-xl'>Men Comfort Cuban Collar Solid Polycotton Casual Shirt...</h1>
+
+                        <Rating name="half-rating-read" defaultValue={4} precision={1} />
+
+                        <div className='px-2 flex items-center py-1 bg-[#f1f1f1] w-fit rounded-md'>
+                            <span onClick={handleClick} className='text-sm flex items-center gap-1 cursor-pointer'>Size: {item.size} <IoMdArrowDropdown /></span>
+
+                            <Menu
+                                id="size-menu"
+                                anchorEl={sizeanchorEl}
+                                open={open}
+                                onClose={() => handleClose(null)}
+
+                            >
+                                {["S", "M", "L", "XL", "XXL"].map((size) => (
+                                    <MenuItem key={size} onClick={()=>handleClose(size)}>
+                                        {size}
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </div>
+
+                        <div className='flex gap-4'>
+                            <p>₹1350</p>
+                            <p className='line-through text-gray-600/70'>₹1450</p>
+                            <p className='text-red-600'>
+                                19% OFF
+                            </p>
+                        </div>
                     </div>
                 </div>
 
-            </section>
+                <div>
+                    <RxCross2 onClick={()=>onremove(item.id)} className='text-2xl cursor-pointer' />
+                </div>
+            </div>
+
+        </section>
     )
 }
 
