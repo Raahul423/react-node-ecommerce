cadeb3b5fd2310bd2e3460f7381ca69858b2ca9f
Raahul423 <rpal24480@gmail.com>
2025-11-11 22:47:34 +0530

    add order model with schema definition

---
+++ Backend/src/Models/order.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+    orderId:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    productId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Product"
+    },
+    prduct_Details:{
+        type:String,
+        Image:Array
+    },
+    paymentId:{
+        type:String,
+        default:""
+    },
+    delivery_address:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Address"
+    },
+    totalAmount:{
+        type:String,
+        default:0
+    }
+},{timestamps:true});
+
+export default Order = mongoose.model("Order",orderSchema);
