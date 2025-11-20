96f58dabe0ded7bff865940f48c6255cac329f6b
Raahul423 <rpal24480@gmail.com>
2025-11-20 13:18:45 +0530

    style: update HomeItems component layout and adjust title styling for consistency

--- Frontend/src/Components/HomeComponent/HomeItems.jsx
+++ Frontend/src/Components/HomeComponent/HomeItems.jsx
@@ -8,13 +8,13 @@ const HomeItems = () => {
     <div className='my-container flex gap-2 justify-between !my-8'>
       {ListItem.map((data, idx) => (
         <Link key={idx}>
-          <div key={idx} className='bg-white flex flex-col justify-center items-center px-6 py-4 gap-2 border-1 border-gray-500/20 ' >
-            <div className='overflow-hidden'>
+          <div key={idx} className='bg-white flex flex-col justify-center items-center gap-2 px-6 py-4 border-1 border-gray-500/20 ' >
+            <div className='overflow-hidden  p-1'>
               <img className='object-contain hover:scale-105 transition-all' src={data.Image} alt="" />
             </div>
 
             <span>
-              <h2 className='text-xl'>{data.Title}</h2>
+              <h2 className='text-md font-mono'>{data.Title}</h2>
             </span>
 
           </div>
