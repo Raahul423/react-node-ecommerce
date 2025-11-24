935084a45fdb937a696c1f0249bf82bcaa66a854
Raahul423 <rpal24480@gmail.com>
2025-11-24 14:19:46 +0530

    add: create product model with schema definition

---
+++ Backend/src/Models/product.model.js
@@ -0,0 +1,95 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    images: [
+      {
+        url: { type: String },
+        publicId: { type: String },
+        required: true,
+      },
+    ],
+    brand: {
+      type: String,
+      default: "",
+    },
+    price: {
+      type: Number,
+      default: 0,
+    },
+    oldprice: {
+      type: Number,
+      default: 0,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    categoryName: {
+      type: String,
+      default: "",
+    },
+    categoryId: {
+      type: string,
+      default: "",
+    },
+    subcategoryName: {
+      type: String,
+      default: "",
+    },
+    subcategoryId: {
+      type: string,
+      default: "",
+    },
+    countInstock: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      default: 0,
+    },
+    isfeatured: {
+      type: Boolean,
+      default: false,
+    },
+    discount: {
+      type: Number,
+      required: true,
+    },
+    productRam: [
+      {
+        type: String,
+        default: null,
+      },
+    ],
+    size: [
+      {
+        type: String,
+        default: null,
+      },
+    ],
+    productWeight: [
+      {
+        type: String,
+        default: null,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+export { Product };
