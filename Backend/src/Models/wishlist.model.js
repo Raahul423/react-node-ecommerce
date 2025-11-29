d70053e56fffcff03d84de49e7e873dc1d10a1a3
Raahul423 <rpal24480@gmail.com>
2025-11-30 02:14:52 +0530

    update wishlist schema

--- Backend/src/Models/wishlist.model.js
+++ Backend/src/Models/wishlist.model.js
@@ -1,36 +1,20 @@
 import mongoose from "mongoose";
 
-const wishListSchema = new mongoose.Schema({
-  brand: {
-    type: String,
-    required: true,
+const wishListSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
   },
-  productId: {
-    type: String,
-    required: true,
-  },
-  userId: {
-    type: String,
-    required: true,
-  },
-  images: {
-    type: String,
-    required: true,
-  },
-  rating: {
-    type: Number,
-    required: true,
-  },
-  price: {
-    type: String,
-    required: true,
-  },
-  oldprice: {
-    type: String,
-    required: true,
-  },
-  discount: {
-    type: Number,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
+
+const Wishlist = mongoose.model("Wishlist", wishListSchema);
+export { Wishlist };
