81467a88cb5efbe317c31ced82fa249f34f5aa39
Raahul423 <rpal24480@gmail.com>
2025-11-30 14:05:21 +0530

    adding remove item from wishlist controller and adding route

--- Backend/src/Controllers/wishlist.controller.js
+++ Backend/src/Controllers/wishlist.controller.js
@@ -1,49 +1,86 @@
 import mongoose from "mongoose";
 import { Wishlist } from "../Models/wishlist.model.js";
 
-
 // AddItem in wishList
-const addItemWishlist = async (req,res) => {
-    try {
-        const userId = req.user?._id
-        if(!userId){
-            throw new Error("Inavlid User");
-        }
-
-        const {productId} = req.body
-        if(!productId){
-            throw new Error("Product id is required....");   
-        }
-
-        const existItem = await Wishlist.findOne({userId,productId});
-        if(existItem){
-            throw new Error("Item Already In your Wishlist..")
-        }
-
-        const newItem = await Wishlist.create({
-            userId,productId
-        })
-
-        return res.status(200).json({success:true,newItem,message:"Successfully Added in WishList"})
-    } catch (error) {
-        return res.status(500).json({success:false,message:error.message})
+const addItemWishlist = async (req, res) => {
+  try {
+    const userId = req.user?._id;
+    if (!userId) {
+      throw new Error("Inavlid User");
     }
-}
+
+    const { productId } = req.body;
+    if (!productId) {
+      throw new Error("Product id is required....");
+    }
+
+    const existItem = await Wishlist.findOne({ userId, productId });
+    if (existItem) {
+      throw new Error("Item Already In your Wishlist..");
+    }
+
+    const newItem = await Wishlist.create({
+      userId,
+      productId,
+    });
+
+    return res
+      .status(200)
+      .json({
+        success: true,
+        newItem,
+        message: "Successfully Added in WishList",
+      });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
 
 //get all wishlist
-const getwishListProduct = async (req,res) => {
-    try {
-        const userId = req.user?._id
-        const wishlistItems = await Wishlist.find({userId}).populate("productId")
-        if(!wishlistItems){
-            throw new Error("Item not found....");
-        }
-
-        return res.status(200).json({success:true,wishlistItems,message:"Successfully fetched all wishList Items"})
-    } catch (error) {
-        return res.status(500).json({success:false,message:error.message})
+const getwishListProduct = async (req, res) => {
+  try {
+    const userId = req.user?._id;
+    const wishlistItems = await Wishlist.find({ userId }).populate("productId");
+    if (!wishlistItems) {
+      throw new Error("Item not found....");
     }
-}
 
+    return res
+      .status(200)
+      .json({
+        success: true,
+        wishlistItems,
+        message: "Successfully fetched all wishList Items",
+      });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
 
-export {addItemWishlist,getwishListProduct}
+// delete wishList
+const removeItem = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const { productId } = req.body;
+    if (!productId) {
+      throw new Error("please provide productId");
+    }
+
+    const deleteItem = await Wishlist.findByIdAndDelete({
+      userId,
+      _id: productId,
+    });
+
+    if (!deleteItem) {
+      throw new Error("Item not found....");
+    }
+
+    return res
+      .status(200)
+      .json({ success: true, message: "Item removed Successfully" });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+export { addItemWishlist, getwishListProduct ,removeItem};
--- Backend/src/Route/wishlist.route.js
+++ Backend/src/Route/wishlist.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { verifyJwt } from "../Middleware/auth.middleware.js";
-import { addItemWishlist, getwishListProduct } from "../Controllers/wishlist.controller.js";
+import { addItemWishlist, getwishListProduct, removeItem } from "../Controllers/wishlist.controller.js";
 const wishlistrouter = Router()
 
 wishlistrouter.route('/add-items').post(verifyJwt,addItemWishlist)
 wishlistrouter.route('/wishlist-products').get(verifyJwt,getwishListProduct)
+wishlistrouter.route('/remove-products').delete(verifyJwt,removeItem)
 
 
 
