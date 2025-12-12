53e47bf427baaa91e7acf9c02ba8df70dab56192
Raahul423 <rpal24480@gmail.com>
2025-12-13 00:11:43 +0530

    complete add product section

--- Frontend/src/Admin/Components/ProductsComponent/Addnewproduct.jsx
+++ Frontend/src/Admin/Components/ProductsComponent/Addnewproduct.jsx
@@ -8,7 +8,9 @@ import Typography from '@mui/material/Typography';
 
 import Slide from '@mui/material/Slide';
 import { IoMdClose } from 'react-icons/io';
-import { FormControl, MenuItem, Select } from '@mui/material';
+import { Button, FormControl, MenuItem, Rating, Select } from '@mui/material';
+import { IoImagesSharp } from 'react-icons/io5';
+import { FaCloudUploadAlt } from 'react-icons/fa';
 
 const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
@@ -59,7 +61,7 @@ export const Addnewproduct = ({ open, setOpen }) => {
                         <IoMdClose />
                     </IconButton>
                     <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
-                        Add New SubCategory
+                        Add New Product
                     </Typography>
                 </Toolbar>
             </AppBar>
@@ -202,6 +204,29 @@ export const Addnewproduct = ({ open, setOpen }) => {
                                 </Select>
                             </FormControl>
                         </span>
+                    </div>
+
+                    <div>
+                        <p>Product Rating</p>
+                        <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
+                    </div>
+
+                    <div className="bg-white shadow-md shadow-gray-600/30 px-10 py-8 rounded-md flex flex-col gap-6">
+
+                        <div className='flex flex-col gap-2'>
+                            <p>Media & Images</p>
+                        </div>
+
+
+                        <div className="h-45 w-45 border border-dashed rounded-md p-1 flex flex-col hover:bg-gray-600/20 cursor-pointer items-center justify-center">
+                            <IoImagesSharp className='h-25 w-25' />
+                            <p>Image Upload</p>
+                        </div>
+
+                        <Button className="!bg-blue-600 !px-8 !py-2 !text-white flex gap-3 cursor-pointer">
+                            <FaCloudUploadAlt className="text-2xl" />
+                            PUBLISH AND VIEW
+                        </Button>
                     </div>
                 </div>
             </List>
