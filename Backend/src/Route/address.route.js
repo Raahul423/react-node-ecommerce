76765a33697c739c1b135127b99bfc4ddd686144
Raahul423 <rpal24480@gmail.com>
2025-12-17 00:16:09 +0530

    created a update address controller function

--- Backend/src/Route/address.route.js
+++ Backend/src/Route/address.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { verifyJwt } from "../Middleware/auth.middleware.js";
-import { addressController } from "../Controllers/address.controller.js";
+import { addressController, updateAddress } from "../Controllers/address.controller.js";
 
 const addressroute = Router();
 
 addressroute.route("/address").post(verifyJwt,addressController);
+addressroute.route("/update-address/:id").put(verifyJwt,updateAddress);
 
 export {addressroute}
