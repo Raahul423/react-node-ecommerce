813141c967bd706811bee8d501663882c655555a
Raahul423 <rpal24480@gmail.com>
2025-12-21 18:43:14 +0530

    remove unused state variables and improve product listing display in AddProduct component

--- Frontend/src/Admin/Components/ProductsComponent/AddProduct.jsx
+++ Frontend/src/Admin/Components/ProductsComponent/AddProduct.jsx
@@ -12,8 +12,7 @@ const AddProduct = () => {
     const [products, setProducts] = useState([])
     const [page, setPage] = useState(0);
     const [rowsPerPage, setRowsPerPage] = useState(10);
-    const [value, setValue] = useState('');
-    const [subcat, setSubcat] = useState('');
+
 
     useEffect(() => {
         const fetchPrdoucts = async () => {
@@ -22,8 +21,8 @@ const AddProduct = () => {
                     `/products/allproducts?page=${page + 1}&limit=${rowsPerPage}`
                 );
 
-                setProducts(res.data.product);       
-                setTotalproduct(res.data.totalProducts); 
+                setProducts(res.data.product);
+                setTotalproduct(res.data.totalProducts);
 
             } catch (error) {
                 if (error?.response) {
@@ -32,30 +31,38 @@ const AddProduct = () => {
                     toastMessage("error", "Server not responding...");
                 }
             }
-            
+
         }
 
         fetchPrdoucts()
     }, [page, rowsPerPage, toastMessage])
-
-
-    const handleChange = (event) => {
-        setValue(event.target.value);
-    };
-
-    const subCategory = (event) => {
-        setSubcat(event.target.value)
-    }
-
 
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
     };
 
     const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(parseInt(event.target.value,10));
+        setRowsPerPage(parseInt(event.target.value, 10));
         setPage(0);
     };
+
+
+    const deleteProduct = async (id) => {
+        try {
+            const res = await api.delete(`/products/delete-product/${id}`);
+
+            setProducts(prev =>
+                prev.filter(cat => cat._id !== id)
+            );
+            toastMessage("success", res?.data?.message);
+        } catch (error) {
+            if (error?.response) {
+                toastMessage("error", error?.response?.data?.message);
+            } else {
+                toastMessage("error", "Server not responding.....")
+            }
+        }
+    }
 
 
     return (
@@ -66,48 +73,11 @@ const AddProduct = () => {
             </div>
 
             <div className='relative border border-gray-600/30  bg-white shadow-md shadow-gray-600/90 rounded-md overflow-x-auto'>
-                <div className='flex px-4 py-6'>
-                    <div className='flex gap-10 '>
-                        <div className='flex flex-col gap-2'>
-                            <p className='!text-xl'>Category By</p>
-                            <FormControl sx={{ m: 1, minWidth: 250 }} size="small">
 
-                                <Select
-                                    value={value}
-                                    onChange={handleChange}
-                                >
-                                    <MenuItem value={10}>Name, A to Z</MenuItem>
-                                    <MenuItem value={20}>Name, Z to A</MenuItem>
-                                    <MenuItem value={30}>Price, Low to High</MenuItem>
-                                    <MenuItem value={30}>Price, High to Low</MenuItem>
-                                </Select>
-                            </FormControl>
-                        </div>
-
-                        <div className='flex flex-col gap-2'>
-                            <p className='!text-xl'>SubCategory By</p>
-                            <FormControl sx={{ m: 1, minWidth: 250 }} size="small">
-                                <Select
-                                    value={subcat}
-                                    onChange={subCategory}
-                                >
-                                    <MenuItem value={10}>Name, A to Z</MenuItem>
-                                    <MenuItem value={20}>Name, Z to A</MenuItem>
-                                    <MenuItem value={30}>Price, Low to High</MenuItem>
-                                    <MenuItem value={30}>Price, High to Low</MenuItem>
-                                </Select>
-                            </FormControl>
-                        </div>
-                    </div>
-
-                    <div>
-
-                    </div>
-                </div>
 
                 <TableContainer sx={{ overflowX: "auto" }} className='scroll'>
                     <Table sx={{ minWidth: 1200 }}>
-                        <TableHead className='sticky top-0'>
+                        <TableHead className='sticky top-0 z-99'>
                             <TableRow className='bg-[#f2f2f2]'>
                                 <TableCell><Checkbox /></TableCell>
                                 <TableCell className='!font-semibold !w-fit'>PRODUCT </TableCell>
@@ -122,46 +92,56 @@ const AddProduct = () => {
 
 
                         <TableBody>
-                            {products.map((row) => (
-                                <TableRow key={row?._id}>
-                                    <TableCell>
-                                        <Checkbox />
-                                    </TableCell>
-                                    <TableCell className='!flex gap-4 '>
-                                        <div>
-                                            <img className='h-17 w-17 rounded-md object-cover' src={row.images[0].url} alt="Error" />
-                                        </div>
+                            {products.length === 0 ?
+                                <tr aria-colspan={8}>
+                                    <p>No Product Found</p>
+                                </tr>
+                                :
+                                <>
+                                    {products.map((row) => (
+                                        <TableRow key={row?._id}>
+                                            <TableCell>
+                                                <Checkbox />
+                                            </TableCell>
+                                            <TableCell className='!flex gap-4 '>
+                                                <div>
+                                                    <img className='h-17 w-17 rounded-md object-cover' src={row.images[0].url} alt="Error" />
+                                                </div>
 
-                                        <div className='flex flex-col gap-1 justify-around'>
-                                            <p className='!font-semibold w-60'>{row.desc}</p>
-                                            <p>{row.name} </p>
-                                        </div>
-                                    </TableCell>
+                                                <div className='flex flex-col gap-1 justify-around'>
+                                                    <p className='!font-semibold w-60'>{row.name}</p>
+                                                    <p>{row.brand} </p>
+                                                </div>
+                                            </TableCell>
 
-                                    <TableCell>{row.category.name}</TableCell>
+                                            <TableCell>{row.category.name}</TableCell>
 
-                                    <TableCell align='center'>{row.subcategory.name}</TableCell>
+                                            <TableCell align='center'>{row.subcategory.name}</TableCell>
 
-                                    <TableCell className='!text-primary !font-semibold' align="center">₹{row.price}</TableCell>
+                                            <TableCell className='!text-primary !font-semibold' align="center">₹{row.price}</TableCell>
 
-                                    <TableCell className='!font-semibold' align="center">
-                                        <Rating
-                                            value={row.rating}
-                                            readOnly
-                                        >
-                                        </Rating>
-                                    </TableCell>
+                                            <TableCell className='!font-semibold' align="center">
+                                                <Rating
+                                                    value={row.rating}
+                                                    readOnly
+                                                >
+                                                </Rating>
+                                            </TableCell>
 
-                                    <TableCell className='!font-semibold !text-primary whitespace-nowrap' align="center">{row.countInstock} stocks</TableCell>
+                                            <TableCell className='!font-semibold !text-primary whitespace-nowrap' align="center">{row.countInstock} stocks</TableCell>
 
 
-                                    <TableCell className='cursor-pointer !font-semibold ' align="center">
-                                        <Button className='!px-4 !py-1 !bg-primary not-visited:'>
-                                            <p className='!text-white'>DELETE</p>
-                                        </Button>
-                                    </TableCell>
-                                </TableRow>
-                            ))}
+                                            <TableCell className='cursor-pointer !font-semibold ' align="center">
+                                                <Button onClick={() => deleteProduct(row._id)} className='!px-4 !py-1 !bg-primary not-visited:'>
+                                                    <p className='!text-white'>DELETE</p>
+                                                </Button>
+                                            </TableCell>
+                                        </TableRow>
+                                    ))}
+                                </>
+                            }
+
+
                         </TableBody>
                     </Table>
                 </TableContainer>
