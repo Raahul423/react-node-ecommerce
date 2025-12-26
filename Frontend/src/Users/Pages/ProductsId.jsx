158c8ae204b27bdeb9020c31118e7989339bc115
Raahul423 <rpal24480@gmail.com>
2025-12-26 20:21:40 +0530

    feat: implement ProductsId component structure; integrate Productdetails, ReviewSection, and ReusableComponents

--- Frontend/src/Users/Pages/ProductsId.jsx
+++ Frontend/src/Users/Pages/ProductsId.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Productdetails from '../Components/ProductIdDetails/Productdetails'
 import ReviewSection from '../Components/ProductIdDetails/ReviewSection'
 import ReuseableComponents from '../Components/ReuseComponent/ReuseableComponents'
 
 
 const ProductsId = () => {
+  const [singleproducts, setSingleproducts] = useState([])
+
+  useEffect(()=>{
+
+  },[]);
   return (
     <section className='my-container'>
       <Productdetails/>
       <ReviewSection/>
-      <ReuseableComponents title={"Related Products"}/>
+      <ReuseableComponents title={"Related Products"} />
     </section>
   )
 }
