e09c025516bf3d913f2cc9a645763e03e5027a81
Raahul423 <rpal24480@gmail.com>
2026-01-06 23:38:55 +0530

    debug mail services

--- Backend/src/Utils/emailotpService.js
+++ Backend/src/Utils/emailotpService.js
@@ -1,13 +1,15 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 dotenv.config();
-import { transporter } from "../Config/mailer.js";
 
-const sendresetPasswordemail = async({ to, otp, name})=>{
-  const verifycode = `${otp}`
+import { Resend } from "resend";
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+const sendresetPasswordemail = async ({ to, otp, name }) => {
+  const verifycode = `${otp}`;
 
   const html = `
     <div style="font-family: Arial, sans-serif; line-height:1.6; color:#333;">
-      <p>Hi ${name || 'there'},</p>
+      <p>Hi ${name || "there"},</p>
       <p>Reset Your Password....</p>
       <p style="text-align:center;">
        <h1>${verifycode}</h1>
@@ -18,14 +20,12 @@ const sendresetPasswordemail = async({ to, otp, name})=>{
     </div>
   `;
 
-  const mailOptions = {
+  await resend.emails.send({
     from: process.env.FROM_EMAIL,
     to,
-    subject: 'Reset Your Password..',
+    subject: "Reset Your Password..",
     html,
-  };
+  });
+};
 
-  return transporter.sendMail(mailOptions); 
-}
-
-export {sendresetPasswordemail}
+export { sendresetPasswordemail };
