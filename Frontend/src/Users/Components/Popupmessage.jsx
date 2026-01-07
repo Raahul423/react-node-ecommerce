85f86baa9a7ce108cbc6d825259919f2d9db0873
Raahul423 <rpal24480@gmail.com>
2026-01-07 15:41:57 +0530

    update pop-up message in project

--- Frontend/src/Users/Components/Popupmessage.jsx
+++ Frontend/src/Users/Components/Popupmessage.jsx
@@ -27,19 +27,24 @@ const Popupmessage = () => {
                 initial={{ y: -500, opacity: 0 }}
                 animate={{ y: 0, opacity: 1 }}
                 transition={{ duration: 1, ease: "easeInOut" }}
-                className='pop-up w-fit fixed top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]'>
+                className='pop-up w-fit fixed top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] '>
 
 
                 <h2 className="md:text-xl text-md font-semibold text-white mb-2">
                     👋 Hi Recruiter!
                 </h2>
                 <p className="text-white mb-4 leading-relaxed">
-                    <span>1.</span>
-                    <p>Google authentication is currently under development.<><span className='text-red-700'>Please use email/password login for now.</span></>  I am  actively working on it.</p>
-                    <span>2.</span>
-                    <p><span className='text-blue-600'>Payment Integration</span> is not Working at the moment. I am currently Working on it. (Testing Payments) </p>
-                    <span>3.</span>
-                    <p>Currently Cart section page is under processing on mobile devices. 😅</p>
+                    <p className='flex gap-2 mb-2'>
+                        <span>1.</span>
+                        <p><span className='text-blue-500'>Google authentication</span> is currently under development.<><span className='text-green-500'>Please use email/password login for now.</span></>  I am  actively working on it.</p>
+                    </p>
+
+                    <p className='flex gap-2 mb-2'> <span>2.</span>
+                        <p><span className='text-red-600'>Payment Integration</span> is not Working at the moment. I am currently Working on it. (Testing Payments) </p></p>
+
+                    <p className='flex  gap-2'><span>3.</span>
+                        <p>Currently <span className='text-red-600'>Cart section</span> page is under processing on mobile devices. 😅</p></p>
+
                 </p>
                 <img className='h-50 rounded-md object-cover' src="/Working Backend.gif" alt="" />
                 <Button
